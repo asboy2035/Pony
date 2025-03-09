d06c42c647a78e761460c7946e9e629786d00328
ash <asboy2035@icloud.com>
2025-03-09 14:10:01 +0100

    Initial Commit

---
+++ Pony/PonyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PonyApp.swift
+//  Pony
+//
+//  Created by ash on 3/9/25.
+//
+
+import SwiftUI
+
+@main
+struct PonyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PonyTests/PonyTests.swift
@@ -0,0 +1,16 @@
+//
+//  PonyTests.swift
+//  PonyTests
+//
+//  Created by ash on 3/9/25.
+//
+
+import Testing
+
+struct PonyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
